2d1e075388914ec042bb6d5601538c4b3d6fcad4
Amir Reza Dalir <dalirnet@gmail.com>
2026-02-23 13:27:56 +0330

    Bump version to 2.0.0 08c2fa3

--- contup.sh
+++ contup.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 # Prebuilt container runtime binaries + CLI management tool for Linux
 # https://github.com/diphyx/contup
 
-CONTUP_VERSION="2.0.0 (2b69284)"
+CONTUP_VERSION="2.0.0 (08c2fa3)"
 GITHUB_REPO="diphyx/contup"
 GITHUB_API="https://api.github.com/repos/${GITHUB_REPO}"
 
